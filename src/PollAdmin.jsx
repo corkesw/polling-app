75ad612d5548437176d54616933d8969ae1d4523
corkesw <stephen.corke@northcoders.com>
2022-04-06 17:56:34 +0100

    add pollAdmin outline

--- src/PollAdmin.jsx
+++ src/PollAdmin.jsx
@@ -1,18 +1,36 @@
 import { get, getDatabase, onValue, ref } from "firebase/database";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import firebaseApp from "./firebase";
 
 const database = getDatabase(firebaseApp);
 
 const PollAdmin = ({ sessionId }) => {
+  const [question, setQuestion] = useState("");
+  const [answers, setAnswers] = useState([]);
+
+  console.log(answers);
   useEffect(() => {
     const path = `data/sessions/${sessionId}/pollData`;
-    console.log(path)
+    console.log(path);
     onValue(ref(database, path), (snapshot) => {
       const data = snapshot.val();
       console.log(data);
+      setQuestion(data.question);
+      setAnswers(() => {
+        const answers = [];
+        for (const key in data.answers) {
+          console.log();
+          answers.push([
+            key,
+            data.answers[key].answer,
+            data.answers[key].isCorrect,
+            data.answers[key].votes,
+          ]);
+        }
+        return answers;
+      });
     });
-  });
+  }, []);
 
   return <div>bananana!</div>;
 };
