cd4b7d607b0354703717ec6a9ebcd16bf98b8d96
corkesw <stephen.corke@northcoders.com>
2022-04-06 17:53:44 +0100

    add user to tutor view

--- src/TutorView.jsx
+++ src/TutorView.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import firebaseApp from "../src/firebase.js";
 import { getDatabase, ref, remove, set } from "firebase/database";
 import CreatePoll from "./CreatePoll.jsx";
 import PollAdmin from "./PollAdmin.jsx";
 import "./styles/TutorView.css";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { Link } from "react-router-dom";
 
 const database = getDatabase(firebaseApp);
+const auth = getAuth(firebaseApp);
 
 const TutorView = () => {
   const [sessionId, setSessionId] = useState("");
   const [sessionName, setSessionName] = useState("");
   const [isQuestion, setIsQuestion] = useState(false);
+  const [isUser, setIsUser] = useState(false);
+  console.log(isUser);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsUser(true);
+      } else {
+        setIsUser(false);
+      }
+    });
+  }, []);
 
   const createDatabaseNodeWithSessionData = (sessionId) => {
     const path = `data/sessions/${sessionId}`;
@@ -23,11 +38,13 @@ const TutorView = () => {
     });
   };
   const clearSession = () => {
-    remove(ref(database, `data/sessions/${sessionId}`)).then(() => {
-      setSessionId("");
-      setIsQuestion(false);
-      setSessionName("");
-    }).catch(err => console.log(err))
+    remove(ref(database, `data/sessions/${sessionId}`))
+      .then(() => {
+        setSessionId("");
+        setIsQuestion(false);
+        setSessionName("");
+      })
+      .catch((err) => console.log(err));
   };
 
   const handleCopyClick = () => {
@@ -39,54 +56,69 @@ const TutorView = () => {
   };
 
   return (
-    <div>
-      {!sessionId ? (
-        <form
-          className="sessionBox"
-          onSubmit={(e) => {
-            const sesString = "_" + Math.random().toString(36).slice(2, 9);
-            e.preventDefault()
-            setSessionId(sesString);
-            createDatabaseNodeWithSessionData(sesString);
-          }}
-        >
-          <label htmlFor="sesName">Session Name: </label>
-          <input
-            onChange={(e) => {
-              setSessionName(e.target.value);
-            }}
-            value={sessionName}
-            id="sesName"
-            type="text"
-          ></input>
-          <button className="sesButton">Start session</button>
-        </form>
-      ) : (
-        <div className="sessionBox">
-          <p>
-            {sessionName} :{" "}
-            <span className="highlightText">
-              localhost:3000/poll/{sessionId}
-            </span>
-            <button onClick={handleCopyClick} className="sesButton">
-              Copy Link
-            </button>
-            <button className="sesButton" type="button" onClick={clearSession}>
-              Clear session
-            </button>
-          </p>
-        </div>
-      )}
+    <>
+      {isUser ? (
+        <div>
+          {!sessionId ? (
+            <form
+              className="sessionBox"
+              onSubmit={(e) => {
+                const sesString = "_" + Math.random().toString(36).slice(2, 9);
+                e.preventDefault();
+                setSessionId(sesString);
+                createDatabaseNodeWithSessionData(sesString);
+              }}
+            >
+              <label className="input" htmlFor="sesName">
+                Session Name:{" "}
+              </label>
+              <input
+                className="input"
+                onChange={(e) => {
+                  setSessionName(e.target.value);
+                }}
+                value={sessionName}
+                id="sesName"
+                type="text"
+              ></input>
+              <button className="sesButton">Start session</button>
+            </form>
+          ) : (
+            <div className="sessionBox">
+              <p>
+                {sessionName} :{" "}
+                <span className="highlightText">
+                  localhost:3000/poll/{sessionId}
+                </span>
+                <button onClick={handleCopyClick} className="sesButton">
+                  Copy Link
+                </button>
+                <button
+                  className="sesButton"
+                  type="button"
+                  onClick={clearSession}
+                >
+                  Clear session
+                </button>
+              </p>
+            </div>
+          )}
 
-      {sessionId && !isQuestion ? (
-        <CreatePoll
-          sessionId={sessionId}
-          isQuestion={isQuestion}
-          setIsQuestion={setIsQuestion}
-        />
-      ) : null}
-      {sessionId && isQuestion ? <PollAdmin sessionId={sessionId} /> : null}
-    </div>
+          {sessionId && !isQuestion ? (
+            <CreatePoll
+              sessionId={sessionId}
+              isQuestion={isQuestion}
+              setIsQuestion={setIsQuestion}
+            />
+          ) : null}
+          {sessionId && isQuestion ? <PollAdmin sessionId={sessionId} /> : null}
+        </div>
+      ) : (
+        <Link className="primaryButton" to="/login">
+          Tutor Login
+        </Link>
+      )}
+    </>
   );
 };
 
