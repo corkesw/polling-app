5b533f2f6d26b4ee64b34f8e37929e0073fd6d9a
corkesw <stephen.corke@northcoders.com>
2022-04-19 11:18:06 +0100

    add conditional rendering of piechart once voting has started

--- src/components/PieMaker.jsx
+++ src/components/PieMaker.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Pie, PieChart } from "recharts";
 import "../styles/PieMaker.css";
 
 const PieMaker = ({ answers }) => {
+  const [hasVotes, setHasVotes] = useState(false);
+  console.log(hasVotes)
+
+  useEffect(() => {
+    const votes = answers.filter((answer) => answer.value > 0);
+    votes.length ? setHasVotes(true) : setHasVotes(false);
+  }, [answers]);
+
   let renderLabel = function (entry) {
-    if (entry.value === 0) return;
+    if (entry.value === 0) {
+      return;
+    }
     const votesCast = answers.reduce((previousAnswer, currentAnswer) => {
       return previousAnswer + currentAnswer.value;
     }, 0);
@@ -16,18 +26,26 @@ const PieMaker = ({ answers }) => {
 
   return (
     <div className="pieChart">
-      <PieChart width={1000} height={500}>
-        <Pie
-          data={answers}
-          dataKey="value"
-          nameKey="name"
-          cx="50%"
-          cy="50%"
-          outerRadius={170}
-          label={renderLabel}
-          labelLine={false}
-        />
-      </PieChart>
+      {hasVotes ? (
+        <PieChart width={1000} height={500}>
+          <Pie
+            data={answers}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={170}
+            label={renderLabel}
+            labelLine={false}
+          />
+        </PieChart>
+      ) : (
+        <img
+          src={require("../images/pie.png")}
+          alt="Pie-holder"
+          width="400"
+        ></img>
+      )}
     </div>
   );
 };
