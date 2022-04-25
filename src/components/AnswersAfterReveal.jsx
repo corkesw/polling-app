38cb1bddf84ce04b2a524aa4e24daef7d77fd194
two-triangles <danielsalehguitar@gmail.com>
2022-04-25 12:45:51 +0100

    qadd CSSTransitionGroup styling

--- src/components/AnswersAfterReveal.jsx
+++ src/components/AnswersAfterReveal.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react"
+import { CSSTransitionGroup } from "react-transition-group"
 
 const AnswersAfterReveal = ({ answers, correctAnswer, hasVoted }) => {
-	// Conditional styling to display the correct answer after the tutor has set reveal to be true
-	return answers.map((answerData, index) => {
-		return answerData.answer === correctAnswer ? (
-			<p key={index}>
-				<button className="correctVoteButton" disabled={hasVoted}>
-					{answerData.answer}
-				</button>
-			</p>
-		) : (
-			<p key={index}>
-				<button className="incorrectVoteButton" disabled={hasVoted}>
-					{answerData.answer}
-				</button>
-			</p>
-		);
-	});
-};
+  // Conditional styling to display the correct answer after the tutor has set reveal to be true
+  return answers.map((answerData, index) => {
+    return answerData.answer === correctAnswer ? (
+      <p key={index}>
+        <CSSTransitionGroup
+          transitionName="showCorrectAnswerTransition"
+          transitionAppear={true}
+          transitionEnter={true}
+        >
+          <button
+            className="correctVoteButton"
+            disabled={true}
+            key={answerData.answer}
+          >
+            {answerData.answer}
+          </button>
+        </CSSTransitionGroup>
+      </p>
+    ) : (
+      <p key={index}>
+        <CSSTransitionGroup
+          transitionName="showIncorrectAnswerTransition"
+          transitionAppear={true}
+          transitionEnter={true}
+        >
+          <button className="unselectedVoteButton" disabled={true}>
+            {answerData.answer}
+          </button>
+        </CSSTransitionGroup>
+      </p>
+    )
+  })
+}
 
-export default AnswersAfterReveal;
+export default AnswersAfterReveal
