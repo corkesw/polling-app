b2e084c4d737273865d647d0e5fb449552e29f5f
two-triangles <danielsalehguitar@gmail.com>
2022-04-26 09:40:11 +0100

    add logic template for transitions

--- src/components/AnswersAfterReveal.jsx
+++ src/components/AnswersAfterReveal.jsx
@@ -1,15 +1,93 @@
 import React, { useEffect, useState } from "react"
-import { CSSTransitionGroup } from "react-transition-group"
+import { CSSTransitionGroup, TransitionGroup } from "react-transition-group"
 
-const AnswersAfterReveal = ({ answers, correctAnswer, hasVoted }) => {
+const AnswersAfterReveal = ({ answers, correctAnswer }) => {
+  const [userAnswer, setUserAnswer] = useState("")
+
+  useEffect(() => {
+    const fetchedUserAnswer = localStorage.getItem("userAnswer")
+    if (fetchedUserAnswer) {
+      setUserAnswer(fetchedUserAnswer)
+    }
+  }, [])
+
+  // console.log(userAnswer, "<-- user answer")
+
   // Conditional styling to display the correct answer after the tutor has set reveal to be true
-  return answers.map((answerData, index) => {
-    return answerData.answer === correctAnswer ? (
+  return (
+    <TransitionGroup>
+      {answers.map((answerData, index) => {
+        //if answer is the correct one
+        return answerData.answer === correctAnswer
+          ? //if its also the users answer
+            answerData.answer === userAnswer
+            ? //purple to green
+              console.log("user correct")
+            : // if not users answer - grey to green
+              console.log("im correct - user silly")
+              //if incorrect answer and user selected it
+          : answerData.answer === userAnswer
+          ? console.log("user selected wrong")
+          //if incorrect answer and ignored
+          : console.log("wasnt picked and is wrong")
+      })}
+    </TransitionGroup>
+  )
+}
+export default AnswersAfterReveal
+
+{
+  /* (answers.map((answerData, index) => {
+    //if correct answer is what user has selected
+    return correctAnswer === userAnswer ? (
+      answerData.answer === correctAnswer ? (
+        <p key={index}>
+          <CSSTransitionGroup
+            transitionName="showCorrectAnswerTransition"
+            transitionAppear={true}
+            transitionEnter={false}
+            transitionLeave={false}
+            transitionAppearTimeout={500}
+            // transitionEnterTimeout={500}
+            // transitionLeaveTimeout={500}
+          >
+            <button
+              className="correctVoteButton"
+              disabled={true}
+              key={answerData.answer}
+            >
+              {answerData.answer}
+            </button>
+          </CSSTransitionGroup>
+        </p>
+      ) : (
+        <p key={index}>
+          <CSSTransitionGroup
+            transitionName={"showIncorrectAnswerNotChosenTransition"}
+            transitionAppear={true}
+            transitionEnter={false}
+            transitionLeave={false}
+            transitionAppearTimeout={500}
+            // transitionEnterTimeout={500}
+            // transitionLeaveTimeout={500}
+          >
+            <button className={"unselectedVoteButton"} disabled={true}>
+              {answerData.answer}
+            </button>
+          </CSSTransitionGroup>
+        </p>
+      )
+    ) : // IF USER DID NOT SELECT RIGHT ANSWER
+    answerData.answer === correctAnswer ? (
       <p key={index}>
         <CSSTransitionGroup
-          transitionName="showCorrectAnswerTransition"
+          transitionName={"correctNotChosenAnswerTransition"}
           transitionAppear={true}
-          transitionEnter={true}
+          transitionEnter={false}
+          transitionLeave={false}
+          transitionAppearTimeout={500}
+          // transitionEnterTimeout={500}
+          // transitionLeaveTimeout={500}
         >
           <button
             className="correctVoteButton"
@@ -23,17 +101,23 @@ const AnswersAfterReveal = ({ answers, correctAnswer, hasVoted }) => {
     ) : (
       <p key={index}>
         <CSSTransitionGroup
-          transitionName="showIncorrectAnswerTransition"
+          transitionName={
+            answerData.answer === userAnswer
+              ? "showIncorrectAnswerButChosenTransition"
+              : "showIncorrectAnswerNotChosenTransition"
+          }
           transitionAppear={true}
-          transitionEnter={true}
+          transitionEnter={false}
+          transitionLeave={false}
+          transitionAppearTimeout={100}
+          // transitionEnterTimeout={500}
+          // transitionLeaveTimeout={500}
         >
-          <button className="unselectedVoteButton" disabled={true}>
+          <button className={"unselectedVoteButton"} disabled={true}>
             {answerData.answer}
           </button>
         </CSSTransitionGroup>
       </p>
     )
-  })
+  }) */
 }
-
-export default AnswersAfterReveal
