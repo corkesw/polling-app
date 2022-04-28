ab87ef1fbc4e3a690eab73a5b4a460b98f16cf50
two-triangles <danielsalehguitar@gmail.com>
2022-04-28 15:52:45 +0100

    made password input of type password

--- src/components/adminComponents/LoginView.jsx
+++ src/components/adminComponents/LoginView.jsx
@@ -45,6 +45,7 @@ const LoginView = () => {
         <label htmlFor="password">password</label>
         <input
           id="password"
+          type={"password"}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
