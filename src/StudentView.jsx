5ae5e42161ba612a437c4c7efcba1c51aaa52c2b
two-triangles <danielsalehguitar@gmail.com>
2022-04-28 15:26:37 +0100

    add overide of inline styling for incorrect answer

--- src/StudentView.jsx
+++ src/StudentView.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
-import firebaseApp from "../src/firebase.js";
-import { getDatabase, ref, onValue, increment, set } from "firebase/database";
-import { useParams } from "react-router-dom";
-import "./styles/StudentView.css";
-import Answers from "./components/Answers.jsx";
-import PieMaker from "./components/PieMaker";
-import colours from "./utils/colours";
+import React, { useEffect, useState } from "react"
+import firebaseApp from "../src/firebase.js"
+import { getDatabase, ref, onValue, increment, set } from "firebase/database"
+import { useParams } from "react-router-dom"
+import "./styles/StudentView.css"
+import Answers from "./components/Answers.jsx"
+import PieMaker from "./components/PieMaker"
+import colours from "./utils/colours"
 
-const database = getDatabase(firebaseApp);
+const database = getDatabase(firebaseApp)
 
 const StudentView = () => {
-  const { seshId } = useParams();
-  const sessionRef = ref(database, `data/sessions/${seshId}`);
+  const { seshId } = useParams()
+  const sessionRef = ref(database, `data/sessions/${seshId}`)
 
-  const [question, setQuestion] = useState("");
-  const [answers, setAnswers] = useState([]);
-  const [answerRevealed, setAnswerRevealed] = useState(false);
-  const [correctAnswer, setCorrectAnswer] = useState("");
-  const [questionId, setQuestionId] = useState("");
-  const [userAnswer, setUserAnswer] = useState("");
-  const [hasVoted, setHasVoted] = useState(false);
-  const [pieData, setPieData] = useState([]);
+  const [question, setQuestion] = useState("")
+  const [answers, setAnswers] = useState([])
+  const [answerRevealed, setAnswerRevealed] = useState(false)
+  const [correctAnswer, setCorrectAnswer] = useState("")
+  const [questionId, setQuestionId] = useState("")
+  const [userAnswer, setUserAnswer] = useState("")
+  const [hasVoted, setHasVoted] = useState(false)
+  const [pieData, setPieData] = useState([])
 
   /*
 	When realtime db updates, set the question state
@@ -30,63 +30,64 @@ const StudentView = () => {
 	*/
 
   useEffect(() => {
-    setAnswerRevealed(false);
-    setUserAnswer("");
-    setHasVoted(false);
+    setAnswerRevealed(false)
+    setUserAnswer("")
+    setHasVoted(false)
 
     onValue(sessionRef, (snapshot) => {
-      const data = snapshot.val();
+      const data = snapshot.val()
 
-      const question = data.pollData.question;
+      const question = data.pollData.question
 
-      const answerCollection = [];
+      const answerCollection = []
       for (const key in data.pollData.answers) {
-        const answerData = data.pollData.answers[key];
-        answerCollection.push(answerData);
-        if (answerData.isCorrect) setCorrectAnswer(answerData.answer);
+        const answerData = data.pollData.answers[key]
+        answerCollection.push(answerData)
+        if (answerData.isCorrect) setCorrectAnswer(answerData.answer)
       }
 
-      const questionId = data.pollData.question_id;
-      const answerRevealed = data.pollData.reveal;
+      const questionId = data.pollData.question_id
+      const answerRevealed = data.pollData.reveal
 
-      setQuestion(question);
-      setAnswers(answerCollection);
-      setAnswerRevealed(answerRevealed);
-      setQuestionId(questionId);
-    });
-  }, [questionId]);
+      setQuestion(question)
+      setAnswers(answerCollection)
+      setAnswerRevealed(answerRevealed)
+      setQuestionId(questionId)
+    })
+  }, [questionId])
 
   useEffect(() => {
     onValue(sessionRef, (snapshot) => {
-      const data = snapshot.val();
+      const data = snapshot.val()
 
       setPieData(() => {
-        const answers = [];
+        const answers = []
         for (const key in data.pollData.answers) {
+          //if the answer is correct and the answers have been revealed
           if (data.pollData.answers[key].isCorrect && answerRevealed) {
             answers.push({
               name: data.pollData.answers[key].answer,
               value: data.pollData.answers[key].votes,
               fill: "#3dd980",
-            });
+            })
           } else if (data.pollData.answers[key].answer === userAnswer) {
             answers.push({
               name: data.pollData.answers[key].answer,
               value: data.pollData.answers[key].votes,
               fill: "#786be4",
-            });
+            })
           } else {
             answers.push({
               name: data.pollData.answers[key].answer,
               value: data.pollData.answers[key].votes,
               fill: `${colours[key]}50`,
-            });
+            })
           }
         }
-        return answers;
-      });
-    });
-  }, [userAnswer, answerRevealed]);
+        return answers
+      })
+    })
+  }, [userAnswer, answerRevealed])
 
   return (
     <div id="container">
@@ -107,11 +108,11 @@ const StudentView = () => {
         <PieMaker
           answers={pieData}
           revealChart={hasVoted}
-		  renderStudentLabel={true}
+          renderStudentLabel={true}
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default StudentView;
+export default StudentView
